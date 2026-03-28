a905f6418f9dcbcbfa6c1219fa300a5750be63d1
Nikita Lipatov <6stringsohei@gmail.com>
2026-03-29 01:16:57 +0300

    feat: add colored terminal rendering for snake and food

--- src/renderer.rs
+++ src/renderer.rs
@@ -23,6 +23,16 @@ impl RenderCell {
     }
 }
 
+const FG_BLACK: &'static str = "\x1b[30m";
+const FG_RED: &'static str = "\x1b[31m";
+const FG_GREEN: &'static str = "\x1b[32m";
+const FG_WHITE: &'static str = "\x1b[97m";
+const BG_BLACK: &'static str = "\x1b[40m";
+const BG_RED: &'static str = "\x1b[41m";
+const BG_GREEN: &'static str = "\x1b[42m";
+const BG_WHITE: &'static str = "\x1b[107m";
+const RESET: &'static str = "\x1b[0m";
+
 pub struct Renderer {}
 
 impl Renderer {
@@ -45,23 +55,62 @@ impl Renderer {
                 };
                 match (top, bottom) {
                     (RenderCell::Empty, RenderCell::Empty) => print!(" "),
-                    (
-                        RenderCell::Food | RenderCell::Wall | RenderCell::Snake,
-                        RenderCell::Empty,
-                    ) => print!("▀"),
-                    (
-                        RenderCell::Empty,
-                        RenderCell::Food | RenderCell::Wall | RenderCell::Snake,
-                    ) => print!("▄"),
-                    (
-                        RenderCell::Food | RenderCell::Wall | RenderCell::Snake,
-                        RenderCell::Food | RenderCell::Wall | RenderCell::Snake,
-                    ) => print!("█"),
+                    (RenderCell::Snake, RenderCell::Snake) => {
+                        self.render_fullbox(FG_GREEN);
+                    }
+                    (RenderCell::Snake, RenderCell::Empty) => {
+                        self.render_halfbox(FG_GREEN, BG_WHITE);
+                    }
+                    (RenderCell::Empty, RenderCell::Snake) => {
+                        self.render_halfbox(FG_WHITE, BG_GREEN);
+                    }
+                    (RenderCell::Wall, RenderCell::Snake) => {
+                        self.render_halfbox(FG_BLACK, BG_GREEN);
+                    }
+                    (RenderCell::Snake, RenderCell::Wall) => {
+                        self.render_halfbox(FG_GREEN, BG_BLACK);
+                    }
+                    (RenderCell::Food, RenderCell::Snake) => {
+                        self.render_halfbox(FG_RED, BG_GREEN);
+                    }
+                    (RenderCell::Snake, RenderCell::Food) => {
+                        self.render_halfbox(FG_GREEN, BG_RED);
+                    }
+                    (RenderCell::Food, RenderCell::Empty) => {
+                        self.render_halfbox(FG_RED, BG_WHITE);
+                    }
+                    (RenderCell::Empty, RenderCell::Food) => {
+                        self.render_halfbox(FG_WHITE, BG_RED);
+                    }
+                    (RenderCell::Food, RenderCell::Wall) => {
+                        self.render_halfbox(FG_RED, BG_BLACK);
+                    }
+                    (RenderCell::Wall, RenderCell::Food) => {
+                        self.render_halfbox(FG_BLACK, BG_RED);
+                    }
+                    (RenderCell::Food, RenderCell::Food) => {
+                        self.render_fullbox(FG_RED);
+                    }
+                    (RenderCell::Wall, RenderCell::Empty) => {
+                        self.render_halfbox(FG_BLACK, BG_WHITE);
+                    }
+                    (RenderCell::Empty, RenderCell::Wall) => {
+                        self.render_halfbox(FG_WHITE, BG_BLACK)
+                    }
+                    (RenderCell::Wall, RenderCell::Wall) => {
+                        self.render_fullbox(FG_BLACK);
+                    }
                 }
             }
             y += 2;
             print!("\r\n")
         }
         std::io::stdout().flush().expect("could not flush stdout");
+    }
+    fn render_halfbox(&self, up_color: &str, bottom_color: &str) {
+        print!("{}{}▀{}", up_color, bottom_color, RESET)
+    }
+    fn render_fullbox(&self, color: &str) {
+        print!("{}█{}", color, RESET)
     }
 }
