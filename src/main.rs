a046de05f5850b7b4bc18e0ce0a28bfa8c11cb28
Nikita Lipatov <6stringsohei@gmail.com>
2026-04-05 20:41:56 +0300

    refactor(game): 2x more food spawn probability

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use snake::terminal::Terminal;
 fn main() {
     let grid = Grid::new(32, 32);
     let snake = Snake::new(Point::new(5, 5));
-    let mut game = Game::new(Terminal::default(), grid, snake, Renderer::new(), 2);
+    let mut game = Game::new(Terminal::default(), grid, snake, Renderer::new(), 4);
     game.start();
     println!("Game over!");
     println!("You've scored: {}", game.score());
